311312e02cc9948487dc8e2d5867a4405ffa7d61
albertusortiz <alberto.ortiz.vargas@gmail.com>
2021-07-01 22:38:48 -0500

    Tipos de anotaciones en Python

---
+++ main.py
@@ -0,0 +1,86 @@
+# Variables
+# Funciones
+# Clases
+# Colecciones
+
+
+
+"""
+Anotaciones en variables
+"""
+""" a: str = 'Hola, esta es una variable'
+b: int = 30
+c: float = 3.1415
+d: bool = True
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+print(d)
+print(type(d)) """
+
+
+
+"""
+Anotaciones en Funciones
+"""
+""" def suma(numero1: int, numero2: int) -> int:
+  return numero1 + numero2
+
+
+valor1: int = 10
+valor2: int = 20
+valor3: int
+
+resultado: int = suma(valor1, valor2)
+
+print(resultado)
+print(type(resultado)) """
+
+
+
+""" 
+Anotaciones en Slases
+"""
+""" class User():
+
+  def __init__(self, username:str, password:str) -> None:
+    self.username = username
+    self.password = password
+
+
+  def saluda(self) -> str:
+    return f'Hola {self.username}'
+
+  def contra(self) -> int:
+    return f'Tu contraseña es: {self.password}'
+
+  
+cody = User('Cody', 12345)
+print(
+  cody.saluda(),
+  cody.contra()
+) """
+
+
+
+""" 
+Anotaciones en Colecciones
+"""
+from typing import List
+
+
+calificaciones: List[int] = [10, 9, 5, 5, 7, 9, 9]
+
+def promedio(calificaciones: List[int]) -> float:
+  return sum(calificaciones) / len(calificaciones)
+
+print(
+  promedio(calificaciones)
+)
