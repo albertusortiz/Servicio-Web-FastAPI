3179b4e8bafc30d86da9457b97f1b9d998807bd6
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-07-05 12:09:53 -0500

    Haciendo validaciones propias con decoradores.

--- pydan.py
+++ pydan.py
@@ -1,18 +1,45 @@
+from pydantic import validator
 from pydantic import BaseModel
+from pydantic import ValidationError
 
 from typing import Optional
+
 
 class User(BaseModel):
   username: str
   password: str
+  repeat_password: str
   email: str
   age: Optional[int] = None
 
-user1 = User(
-  username=10,
-  password='password123',
-  email='info@codigofacilito.com',
-  age=27
-)
+  @validator('username')
+  def username_validation_length(cls, username):
+    
+    if len(username) < 3:
+      raise ValueError('La longitud minima es de 4 caracteres.')
 
-print(user1)
+    if len(username) > 50:
+      raise ValueError('La longitud máxima es de 50 caracteres.')
+
+    return username
+
+  @validator('repeat_password')
+  def repeat_password_validation(cls, repeat_password, values):
+    
+    if 'password' in values and repeat_password != values['password']:
+      raise ValidationError('Las contraseñas son diferentes.')
+
+    return repeat_password
+
+try:
+  user1 = User(
+    username='Cody',
+    password='password123',
+    repeat_password='password123',
+    email='info@codigofacilito.com',
+    age=27
+  )
+
+  print(user1)
+except ValidationError as e:
+  print(e.json())
