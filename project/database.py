1d4e9ea1c67ca9cf66b74986c7cae4ce248a3b67
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-07-05 15:37:56 -0500

    Creando modelos de la aplicacion

--- project/database.py
+++ project/database.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 from peewee import *
+from datetime import datetime
 
 database = MySQLDatabase(
     'albertus_servicio_fast_api',
@@ -7,3 +9,30 @@ database = MySQLDatabase(
     host='162.241.61.214',
     port=3306
 )
+
+class User(Model):
+    username = CharField(max_length=50, unique=True)
+    password = CharField(max_length=50)
+    created_at = DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        database = database
+        table_name = 'users'
+
+class Movie(Model):
+    title = CharField(max_length=50)
+    created_at = DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        database=database
+        table_name = 'movies'
+
+class UserReviuew(Model):
+    user = ForeignKeyField(User, backref='reviews')
+    movie = ForeignKeyField(Movie, backref)
