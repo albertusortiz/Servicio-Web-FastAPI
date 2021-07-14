01ce84805c8f27373201040a756a05a64b855707
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-07-14 14:22:32 -0500

    Peticion via POST

--- project/client.py
+++ project/client.py
@@ -1,6 +1,8 @@
 import requests
 from requests.api import head
 
+""" # GET
+
 URL = 'http://127.0.0.1:8000/api/v1/reviews'
 HEADERS = { 'accept': 'application/json' }
 QUERYSET= { 'page': 1, 'limit': 2 }
@@ -19,4 +21,26 @@ if response.status_code == 200:
         reviews = response.json()
 
         for review in reviews:
-            print(f'> score: {review["score"]} - {review["review"]}')
+            print(f'> score: {review["score"]} - {review["review"]}') """
+
+
+# POST
+
+URL = 'http://127.0.0.1:8000/api/v1/reviews'
+REVIEW = {
+    "user_id": 1,
+    "movie_id": 1,
+    "review": "Review creada con request.",
+    "score": 2
+}
+
+response = requests.post(URL, json=REVIEW)
+
+if response.status_code == 200:
+    print('Reseña creada de forma exitosa.')
+
+    print(response.json()["id"])
+else:
+    print(
+        response.content
+    )
