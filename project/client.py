2a54a38202388d75ff28bab14899e0f77b67eabf
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-07-14 12:49:29 -0500

    Envio de parametros con la propiedad params mediante un QUERYSET

--- project/client.py
+++ project/client.py
@@ -3,8 +3,9 @@ from requests.api import head
 
 URL = 'http://127.0.0.1:8000/api/v1/reviews'
 HEADERS = { 'accept': 'application/json' }
+QUERYSET= { 'page': 1, 'limit': 2 }
 
-response = requests.get(URL, headers=HEADERS)
+response = requests.get(URL, headers=HEADERS, params=QUERYSET)
 
 if response.status_code == 200:
     print("Peticion realizada de forma exitosa.")
